23b327a261425e4d4fbdd47adda93431593bb091
ashwyn28 <ashwyn28annauth@gmail.com>
2022-07-18 23:15:20 +0100

    hero banner + dark mode

---
+++ src/components/hero.svelte
@@ -0,0 +1,79 @@
+<script lang="ts">
+	import { Form, Input } from 'sveltejs-forms';
+</script>
+
+<div>
+	<p class="text-center text-4xl dark:text-white font-bold text-black">
+		Find the right place for you <i class="pl-2 fa-solid fa-user-astronaut" />
+	</p>
+	<p class="text-center text-slate-400">
+		Our job board is curated to provide you the best work from home opportunities.
+	</p>
+	<div class="text-center">
+		<Form>
+			<div class="flex flex-row justify-center">
+				<Input class="text-sm rounded" name="email" placeholder="Enter your email.." value="" />
+				<button
+					type="submit"
+					class="ml-1 px-4 py-0 my-0 bg-send-green text-white hover:text-send-green hover:bg-white dark:hover:bg-slate-800 rounded invisible sm:visible text-sm font-semibold hover:ring-2 hover:ring-send-green"
+					>Get Started</button
+				>
+			</div>
+		</Form>
+	</div>
+</div>
+
+<style>
+	:global(.sveltejs-forms) {
+		padding: 1rem;
+		border-radius: 5px;
+	}
+
+	:global(label) {
+		font-size: 0.8rem;
+		color: #888;
+		margin-bottom: 0.2rem;
+	}
+
+	:global(.message) {
+		font-size: 0.8rem;
+		color: #ffff;
+		margin: 0.2rem 0;
+		color: #f56565;
+	}
+
+	:global(input[type='text']),
+	:global(textarea),
+	:global(select) {
+		height: 30px;
+		background-color: white;
+		margin: 0;
+	}
+
+	:global(input[type='checkbox'] + label) {
+		display: inline-block;
+		margin-right: 2rem;
+	}
+
+	:global(.field) {
+		margin-bottom: 1rem;
+	}
+
+	.buttons {
+		margin-top: 0.8rem;
+		display: flex;
+		align-items: center;
+	}
+
+	button[type='reset'] {
+		background-color: #f56565;
+	}
+
+	button[type='submit'] {
+		height: 30px;
+	}
+
+	.debugger {
+		margin-top: 1rem;
+	}
+</style>
--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -36,20 +36,30 @@
 	});
 </script>
 
+<svelte:head>
+	<script src="https://kit.fontawesome.com/4c5b5990de.js" crossorigin="anonymous"></script>
+</svelte:head>
 <header class="flex p-4">
 	<div class="ml-auto">
 		{#if $page.url.pathname === '/listings'}
 			<a href="listing/post/">
 				<button
-					class="px-4 py-1 bg-red-500 text-white hover:text-red-500 hover:bg-white rounded invisible sm:visible text-lg font-semibold hover:ring-2 hover:ring-red-500"
+					class="px-4 py-1 bg-send-green text-white hover:text-send-green hover:bg-white dark:hover:bg-slate-800 rounded invisible sm:visible text-lg font-semibold hover:ring-2 hover:ring-send-green"
 					>Post</button
 				>
 			</a>
-			<button
-				on:click={() => {
-					toggleDarkMode((darkMode = !darkMode));
-				}}>dark mode</button
-			>
 		{/if}
+		<button
+			class="text-xl dark:text-white pl-4"
+			on:click={() => {
+				toggleDarkMode((darkMode = !darkMode));
+			}}>
+			{#if darkMode}
+				<i class="fa-solid fa-moon" />
+			{:else}
+				<i class="fa-solid fa-sun" />
+			{/if}
+			</button
+		>
 	</div>
 </header>
